f42037678ea2de8815d74e17dd47b5822242237b
unknown <leonardotrx@outlook.com>
2026-04-09 20:28:11 -0300

    add: usuariomodel

---
+++ src/main/java/org/cesumar/models/solicitacao/UsuarioModel.java
@@ -0,0 +1,70 @@
+package org.cesumar.models.solicitacao;
+
+import java.util.UUID;
+
+public class UsuarioModel {
+
+    private UUID id;
+    private String nome;
+    private Long idade;
+    private String logradouro;
+    private String numeroLogradouro;
+    private String cep;
+
+    public UsuarioModel(UUID id, String nome, Long idade, String logradouro, String numeroLogradouro, String cep) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.logradouro = logradouro;
+        this.numeroLogradouro = numeroLogradouro;
+        this.cep = cep;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Long idade) {
+        this.idade = idade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumeroLogradouro() {
+        return numeroLogradouro;
+    }
+
+    public void setNumeroLogradouro(String numeroLogradouro) {
+        this.numeroLogradouro = numeroLogradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
